819c4d038f8a13ab41b13cf37845b4aaeab2a2cb
Dean Araujo <deantarik4@gmail.com>
2024-05-07 17:40:12 -0300

    Lista Duplamente Encadeada (pt 6 - toString)

--- src/main/java/com/projeto/listaduplamenteencadeada/one/digitalinnovation/ListaDuplamenteEncadeada.java
+++ src/main/java/com/projeto/listaduplamenteencadeada/one/digitalinnovation/ListaDuplamenteEncadeada.java
@@ -83,4 +83,17 @@ public class ListaDuplamenteEncadeada<T> {
         }
         this.tamanhoLista--;
     }
+
+    @Override
+    public String toString(){
+        String strRetorno = "";
+
+        NoDuplo<T> noAuxiliar = primeiroNo;
+        for (int i = 0; i < size(); i++){
+            strRetorno += "[No{conteudo=" + noAuxiliar.getConteudo() + "}]-->";
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        strRetorno += "null";
+        return strRetorno;
+    }
 }
